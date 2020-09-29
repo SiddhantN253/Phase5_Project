8fe77f73d138df354d4930ee1b6fe87a71a048bf
snalawade <snalawade@10.217.199.121>
2020-09-29 19:01:04 +0530

    Commit

---
+++ SpringHelloAWS/src/main/java/com/hello/SpingBootHelloWorldApplication.java
@@ -0,0 +1,13 @@
+package com.hello;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpingBootHelloWorldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpingBootHelloWorldApplication.class, args);
+	}
+
+}
---
+++ SpringHelloAWS/src/main/java/com/hello/controller/HelloController.java
@@ -0,0 +1,15 @@
+package com.hello.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+	
+	@GetMapping("/")
+	public String Hello() {
+		return "Welcome to SpringBoot";
+	}
+	
+}
